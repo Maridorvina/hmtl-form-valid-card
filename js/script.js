af195a28d5b645cfde6a8ba13d8bf2716ccc271a
maridrv <marihd10@gmail.com>
2019-03-31 23:13:28 -0300

    arquivo js adicionado

---
+++ js/script.js
@@ -0,0 +1,18 @@
+titulo.addEventlistener("click", mostraMensagem);
+
+function mostraMensagem(){
+    console.log("Olá eu fui clicado!");
+}
+
+// var botaoAdicionar = document.querySelector("#adicionar-cartao"); //Propriedade de botão para adicionar o cartão na tela.
+// boataoAdicionar.addEventListener("click", function(event){
+//     event.preventDefault();
+
+//     var span = document.querySelector("");
+
+// })
+
+// titulo.addEventListener("click", function () {
+//     console.log("fui clicado");
+// });
+
